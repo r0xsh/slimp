f1ecf10b43e409f777214f372056172d95115d73
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-04-12 15:39:53 +0200

    format

--- src/app.js
+++ src/app.js
@@ -1,29 +1,31 @@
-import express, { json } from 'express'
-import sling from './sling.js'
+import express, { json } from "express";
+import sling from "./sling.js";
 
-const app = express()
-const port = 7676
+const app = express();
+const port = 7676;
 
-app.use(json())
+app.use(json());
 
-app.post('/csv', async (req, res) => {
-    const {token, from, to, template, per_x_min} = req.body
-    let cal = await sling.getCalendar(token, new Date(from), new Date(to))
-    if (template === 'per_x_min') {
-        cal = sling.subDividDays(cal, per_x_min)
-    }
+app.post("/csv", async (req, res) => {
+  const { token, from, to, template, per_x_min } = req.body;
+  let cal = await sling.getCalendar(token, new Date(from), new Date(to));
+  if (template === "per_x_min") {
+    cal = sling.subDividDays(cal, per_x_min);
+  }
 
-    const csv = sling.toCSV(cal)
-    
-    res.setHeader("Content-Type", "text/csv")
-    res.setHeader("Content-Disposition", "attachment;filename=export.csv")
-    res.writeHead(200)
-    res.end(csv)
-})
+  const csv = sling.toCSV(cal);
+
+  res.setHeader("Content-Type", "text/csv");
+  res.setHeader("Content-Disposition", "attachment;filename=export.csv");
+  res.writeHead(200);
+  res.end(csv);
+});
 
 // Used for production
-app.get('/privacy.html', (_req, res) => res.sendFile('/usr/src/app/privacy.html'))
+app.get("/privacy.html", (_req, res) =>
+  res.sendFile("/usr/src/app/privacy.html"),
+);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
--- src/sling.js
+++ src/sling.js
@@ -1,212 +1,221 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 async function fetchData(url, token) {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': token
-        }
-    });
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
 
-    return await response.json();
+  return await response.json();
 }
 
 async function fetchConcise(session) {
-    const url = `https://api.getsling.com/v1/${session.org.id}/users/concise?user-fields=full`;
-    return await fetchData(url, session.token);
+  const url = `https://api.getsling.com/v1/${session.org.id}/users/concise?user-fields=full`;
+  return await fetchData(url, session.token);
 }
 
 async function fetchSession(token) {
-    const url = "https://api.getsling.com/v1/account/session";
-    return await fetchData(url, token);
+  const url = "https://api.getsling.com/v1/account/session";
+  return await fetchData(url, token);
 }
 
 async function fetchCalendar(session, from, to) {
-    to.setHours(23, 59, 59);
-    const url = `https://api.getsling.com/v1/${session.org.id}/calendar/${session.org.id}/users/${session.id}?dates=${from.toISOString()}/${to.toISOString()}`;
-    return await fetchData(url, session.token);
+  to.setHours(23, 59, 59);
+  const url = `https://api.getsling.com/v1/${session.org.id}/calendar/${session.org.id}/users/${session.id}?dates=${from.toISOString()}/${to.toISOString()}`;
+  return await fetchData(url, session.token);
 }
 
-
 function getUserById(users, id) {
-    return users.find(user => user.id == id)
+  return users.find((user) => user.id == id);
 }
 
 function getGroupById(groups, id) {
-    return groups.find(group => group.id == id)
+  return groups.find((group) => group.id == id);
 }
-
 
 // Sorting functions
 function _grpSort(a, b) {
-    return a.name > b.name ? 1 : -1
+  return a.name > b.name ? 1 : -1;
 }
 
 function _dateSort(a, b) {
-    return a > b ? 1 : -1
+  return a > b ? 1 : -1;
 }
 
 function _userSort(a, b) {
-    return a.id > b.id ? 1 : -1
+  return a.id > b.id ? 1 : -1;
 }
 
 function _calSort(a, b) {
-    if (a.user.id != b.user.id) {
-        return _userSort(a.user, b.user)
-    }
-    if (a.day != b.day) {
-        return _dateSort(a.day, b.day)
-    }
-    return 0
+  if (a.user.id != b.user.id) {
+    return _userSort(a.user, b.user);
+  }
+  if (a.day != b.day) {
+    return _dateSort(a.day, b.day);
+  }
+  return 0;
 }
 
 function delta_worked_in_timerange(shift, from, to) {
-    let delta = to - from
+  let delta = to - from;
 
-    if (shift.end < to) {
-        delta -= (to - shift.end)
-    }
+  if (shift.end < to) {
+    delta -= to - shift.end;
+  }
 
-    if (shift.start > from) {
-        delta -= (shift.start - from)
-    }
+  if (shift.start > from) {
+    delta -= shift.start - from;
+  }
 
-    return delta / 60000
+  return delta / 60000;
 }
-
 
 // State building
 async function getCalendar(token, from, to) {
-    // Fetch session details
-    const sessionData = await fetchSession(token);
-    const session = {
-        token: token,
-        id: sessionData.user.id,
-        firstname: sessionData.user.name,
-        lastname: sessionData.user.lastname,
-        org: {
-            id: sessionData.org.id,
-            name: sessionData.org.name,
-        },
-    };
+  // Fetch session details
+  const sessionData = await fetchSession(token);
+  const session = {
+    token: token,
+    id: sessionData.user.id,
+    firstname: sessionData.user.name,
+    lastname: sessionData.user.lastname,
+    org: {
+      id: sessionData.org.id,
+      name: sessionData.org.name,
+    },
+  };
 
-    // Build users list
-    const conciseUserData = await fetchConcise(session);
-    const users = conciseUserData.users.map(user => ({
-        id: user.id,
-        firstname: user.name,
-        lastname: user.lastname
-    })).sort(_userSort);
+  // Build users list
+  const conciseUserData = await fetchConcise(session);
+  const users = conciseUserData.users
+    .map((user) => ({
+      id: user.id,
+      firstname: user.name,
+      lastname: user.lastname,
+    }))
+    .sort(_userSort);
 
-    // Build groups list
-    const groups = Object.entries(conciseUserData.groups).map(([_, group]) => ({
-        id: group.id,
-        name: group.name
-    })).sort(_grpSort);
+  // Build groups list
+  const groups = Object.entries(conciseUserData.groups)
+    .map(([_, group]) => ({
+      id: group.id,
+      name: group.name,
+    }))
+    .sort(_grpSort);
 
-    // Fetch and process calendar data
-    const calendarData = await fetchCalendar(session, from, to);
-    const processedCalendar = calendarData
-    .filter(event => event.type === 'shift')
-    .filter(event => event.hasOwnProperty('user')) // Filter out shifts without user
-    .map(event => {
-        const start = dayjs(event.dtstart)
-        const end = dayjs(event.dtend)
-        const day = dayjs(start).startOf('day')
+  // Fetch and process calendar data
+  const calendarData = await fetchCalendar(session, from, to);
+  const processedCalendar = calendarData
+    .filter((event) => event.type === "shift")
+    .filter((event) => event.hasOwnProperty("user")) // Filter out shifts without user
+    .map((event) => {
+      const start = dayjs(event.dtstart);
+      const end = dayjs(event.dtend);
+      const day = dayjs(start).startOf("day");
 
-        return {
-            id: event.id,
-            day: day.toDate(),
-            notes: event.assigneeNotes,
-            start: start.toDate(),
-            end: end.toDate(),
-            delta: end.diff(start, 'minute'),
-            user: getUserById(users, event.user.id),
-            task: getGroupById(groups, event.position.id)
-        };
-    }).sort(_calSort);
+      return {
+        id: event.id,
+        day: day.toDate(),
+        notes: event.assigneeNotes,
+        start: start.toDate(),
+        end: end.toDate(),
+        delta: end.diff(start, "minute"),
+        user: getUserById(users, event.user.id),
+        task: getGroupById(groups, event.position.id),
+      };
+    })
+    .sort(_calSort);
 
-    return {
-        data: processedCalendar,
-        session, groups, users
-    }
+  return {
+    data: processedCalendar,
+    session,
+    groups,
+    users,
+  };
 }
 
 function subDividDays(cal, subDivideBy) {
-    const divided = cal.data.reduce((acc, shift) => {
-        for (let date = shift.start; date < shift.end; date.setMinutes(date.getMinutes() + subDivideBy)) {
-            const end = new Date(date.getTime() + subDivideBy * 60000);
-            const start = new Date(date)
-            acc.push({
-                ...shift,
-                day: start,
-                start, end,
-                delta: delta_worked_in_timerange(shift, date, end)
-            })
-        }
-        return acc
-    }, [])
+  const divided = cal.data.reduce((acc, shift) => {
+    for (
+      let date = shift.start;
+      date < shift.end;
+      date.setMinutes(date.getMinutes() + subDivideBy)
+    ) {
+      const end = new Date(date.getTime() + subDivideBy * 60000);
+      const start = new Date(date);
+      acc.push({
+        ...shift,
+        day: start,
+        start,
+        end,
+        delta: delta_worked_in_timerange(shift, date, end),
+      });
+    }
+    return acc;
+  }, []);
 
-    return { ...cal, data: divided }
-
+  return { ...cal, data: divided };
 }
 
 function toCSV(data, div) {
-    if (!data || data.data.length === 0) {
-        return '';
+  if (!data || data.data.length === 0) {
+    return "";
+  }
+
+  // Extract group names and prepare the header
+  const groupNames = data.groups.map((group) => group.name);
+  const firstCols = ["Name", "Day", "Start", "End"];
+  let csvLines = [[...firstCols, ...groupNames].join(",")]; // Header row
+
+  let previousUser = "",
+    previousDay = "";
+  let currentRow = [];
+
+  for (const record of data.data) {
+    const isNewUser = previousUser !== record.user.id;
+    const isNewDay = previousDay !== record.day;
+
+    // Start a new row for a different user or day
+    if (isNewUser || isNewDay) {
+      // Add the current row to CSV lines if it's not empty
+      if (currentRow.length > 0) {
+        csvLines.push(currentRow.join(","));
+      }
+
+      previousUser = record.user.id;
+      previousDay = record.day;
+      currentRow = Array(groupNames.length + firstCols.length).fill("");
     }
 
-    // Extract group names and prepare the header
-    const groupNames = data.groups.map(group => group.name);
-    const firstCols = ['Name', 'Day', 'Start', 'End'];
-    let csvLines = [[...firstCols, ...groupNames].join(',')]; // Header row
+    // Fill the row with user details and day
+    currentRow[0] = record.user.firstname + " " + record.user.lastname;
+    currentRow[1] = record.day.toLocaleDateString();
+    currentRow[2] = record.start.toLocaleTimeString();
+    currentRow[3] = record.end.toLocaleTimeString();
 
-    let previousUser = '', previousDay = '';
-    let currentRow = [];
-
-    for (const record of data.data) {
-        const isNewUser = previousUser !== record.user.id;
-        const isNewDay = previousDay !== record.day;
-
-        // Start a new row for a different user or day
-        if (isNewUser || isNewDay) {
-            // Add the current row to CSV lines if it's not empty
-            if (currentRow.length > 0) {
-                csvLines.push(currentRow.join(','));
-            }
-
-            previousUser = record.user.id;
-            previousDay = record.day;
-            currentRow = Array(groupNames.length + firstCols.length).fill('');
-        }
-
-        // Fill the row with user details and day
-        currentRow[0] = record.user.firstname + ' ' + record.user.lastname;
-        currentRow[1] = record.day.toLocaleDateString();
-        currentRow[2] = record.start.toLocaleTimeString();
-        currentRow[3] = record.end.toLocaleTimeString();
-
-        // Update the specific column for the task
-        const taskColumnIndex = groupNames.indexOf(record.task.name) + firstCols.length;
-        if (Number.isInteger(currentRow[taskColumnIndex])) {
-            currentRow[taskColumnIndex] += record.delta;
-        } else {
-            currentRow[taskColumnIndex] = record.delta;
-        }
+    // Update the specific column for the task
+    const taskColumnIndex =
+      groupNames.indexOf(record.task.name) + firstCols.length;
+    if (Number.isInteger(currentRow[taskColumnIndex])) {
+      currentRow[taskColumnIndex] += record.delta;
+    } else {
+      currentRow[taskColumnIndex] = record.delta;
     }
+  }
 
-    // Add the last row to CSV lines
-    if (currentRow.length > 0) {
-        csvLines.push(currentRow.join(','));
-    }
+  // Add the last row to CSV lines
+  if (currentRow.length > 0) {
+    csvLines.push(currentRow.join(","));
+  }
 
-    return csvLines.join('\n');
+  return csvLines.join("\n");
 }
-
 
 export default {
-    getCalendar,
-    subDividDays,
-    toCSV
-}
+  getCalendar,
+  subDividDays,
+  toCSV,
+};
